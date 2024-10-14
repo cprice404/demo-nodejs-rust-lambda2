momentohq/demo-nodejs-rust-lambda

feat: files for milestone4, initial napi-rs crate (#9)

lambdas/rust-workspace/napi_rs_momento_cache/index.d.ts
@@ -1,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+
+/* auto-generated by NAPI-RS */
+
+export declare class NapiRsMomentoCache {
+  static create(logLevel: string, cacheName: string, momentoApiKey: string): NapiRsMomentoCache
+  store(key: string, value: string): Promise<void>
+  retrieve(key: string): Promise<string | null>
+}

lambdas/rust-workspace/napi_rs_momento_cache/src/lib.rs
@@ -1,0 +1,32 @@
+#![deny(clippy::all)]
+
+#[macro_use]
+extern crate napi_derive;
+
+use momento_cache_facade::initialize_logging;
+
+#[napi]
+pub struct NapiRsMomentoCache {
+  momento_cache: momento_cache_facade::MomentoCacheFacade
+}
+
+#[napi]
+impl NapiRsMomentoCache {
+  #[napi(factory)]
+  pub fn create(log_level: String, cache_name: String, momento_api_key: String) -> Self {
+    initialize_logging(&log_level);
+    NapiRsMomentoCache {
+      momento_cache: momento_cache_facade::MomentoCacheFacade::new(cache_name, momento_api_key)
+    }
+  }
+
+  #[napi]
+  pub async fn store(&self, key: String, value: String) {
+    self.momento_cache.store(&key, &value).await
+  }
+
+  #[napi]
+  pub async fn retrieve(&self, key: String) -> Option<String> {
+      self.momento_cache.retrieve(&key).await
+  }
+}

cprice404/demo-nodejs-rust-lambda2

lambdas/rust-workspace/napi_rs_momento_cache/index.d.ts
@@ -1,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+
+/* auto-generated by NAPI-RS */
+
+export declare class NapiRsMomentoCache {
+  static create(logLevel: string, momentoApiKey: string, cacheName: string): NapiRsMomentoCache
+  store(key: string, value: string): Promise<void>
+  retrieve(key: string): Promise<string | null>
+}

lambdas/rust-workspace/napi_rs_momento_cache/src/lib.rs
@@ -1,0 +1,32 @@
+#![deny(clippy::all)]
+
+#[macro_use]
+extern crate napi_derive;
+
+use momento_cache_facade::initialize_logging;
+
+#[napi]
+pub struct NapiRsMomentoCache {
+  momento_cache: momento_cache_facade::MomentoCacheFacade
+}
+
+#[napi]
+impl NapiRsMomentoCache {
+  #[napi(factory)]
+  pub fn create(log_level: String, momento_api_key: String, cache_name: String) -> Self {
+    initialize_logging(&log_level);
+    NapiRsMomentoCache {
+      momento_cache: momento_cache_facade::MomentoCacheFacade::new(momento_api_key, cache_name)
+    }
+  }
+
+  #[napi]
+  pub async fn store(&self, key: String, value: String) {
+    self.momento_cache.store(&key, &value).await
+  }
+
+  #[napi]
+  pub async fn retrieve(&self, key: String) -> Option<String> {
+      self.momento_cache.retrieve(&key).await
+  }
+}